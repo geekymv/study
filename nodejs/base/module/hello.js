a2ac919bef81d7f0cc6198f349315fff0933fec0
geekymv <ym2011678@foxmail.com>
2016-12-16 14:03:37 +0800

    module demo

---
+++ nodejs/base/module/hello.js
@@ -0,0 +1,9 @@
+/**
+ * Created by geekymv on 2016/12/16.
+ */
+'use strict'
+var s = 'hello';
+function greet(name) {
+    console.log(s + ',' + name + '!');
+}
+module.exports = greet;
---
+++ nodejs/base/module/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by geekymv on 2016/12/16.
+ */
+'use strict'
+
+var greet = require('./hello');
+var name = 'Tom';
+
+greet(name);
+
