e147ffbe31619676ddd9372474c1b3e6205a08aa
geekymv <ym2011678@foxmail.com>
2016-12-16 14:15:37 +0800

    module fs

---
+++ nodejs/base/fs/readFile.js
@@ -0,0 +1,13 @@
+/**
+ * Created by geekymv on 2016/12/16.
+ */
+'use strict'
+var fs = require('fs');
+
+fs.readFile('sample.txt', 'utf-8', function(err, data) {
+    if(err) {
+        console.log(err);
+    }else {
+        console.log(data);
+    }
+});
